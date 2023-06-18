9ad6bd2343a8462f36373666c2e3dd29f3efbaee
sultanyusufalbari@gmail.com <sultanyusufalbari@gmail.com>
2023-06-18 21:50:12 +0700

    final proyek 12 nuxt x vue js

---
+++ app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <FormSiswa @siswa-added="updateDaftarSiswa"></FormSiswa>
+    <DaftarSiswa :siswa-baru="siswaBaru"></DaftarSiswa>
+  </div>
+</template>
+
+<script>
+import FormSiswa from './component/FormSiswa';
+import DaftarSiswa from './component/DaftarSiswa';
+
+export default {
+  components: {
+    FormSiswa,
+    DaftarSiswa
+  },
+  data() {
+    return {
+      siswaBaru: null
+    };
+  },
+  methods: {
+    updateDaftarSiswa(siswa) {
+      this.siswaBaru = siswa;
+    }
+  }
+};
+</script>
---
+++ component/DaftarSiswa.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+      <h2>Daftar Siswa</h2>
+      <ul>
+        <li v-for="(siswa, index) in daftarSiswa" :key="index">
+          No Induk: {{ siswa.induk }} - Nama: {{ siswa.nama }} - Nilai: {{ siswa.nilai }}
+        </li>
+      </ul>
+      <h3>Rata-rata Nilai Tugas: {{ rataRataNilai }}</h3>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      siswaBaru: {
+        type: Object,
+        required: true
+      }
+    },
+    data() {
+      return {
+        daftarSiswa: []
+      };
+    },
+    computed: {
+      rataRataNilai() {
+        if (this.daftarSiswa.length === 0) {
+          return 0;
+        }
+        const totalNilai = this.daftarSiswa.reduce((sum, siswa) => sum + siswa.nilai, 0);
+        return (totalNilai / this.daftarSiswa.length).toFixed(2);
+      }
+    },
+    watch: {
+      siswaBaru(newSiswa) {
+        this.daftarSiswa.push(newSiswa);
+      }
+    }
+  };
+  </script>
+  
