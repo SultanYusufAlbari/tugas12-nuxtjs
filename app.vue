7df0ed2d43dd207a729aad30af4576cb78d31386
sultanyusufalbari@gmail.com <sultanyusufalbari@gmail.com>
2023-06-18 21:55:28 +0700

    final proyek nuxt x vue js

--- app.vue
+++ app.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+//import 'bootstrap/dist/css/bootstrap.css'
+//import 'bootstrap-vue/dist/bootstrap-vue.css'
 import FormSiswa from './component/FormSiswa';
 import DaftarSiswa from './component/DaftarSiswa';
 
